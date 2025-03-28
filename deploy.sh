7581e838870c81e0283fe352cb408dd29ded513e
pedroortizsolis <portiz@territoriosinteligentes.net>
2025-03-28 09:58:47 -0500

    New: Microservice bank-clients now running, tests passed

--- deploy.sh
+++ deploy.sh
@@ -4,7 +4,10 @@
 IMAGE_NAME="bank-clients"
 IMAGE_VERSION="latest"
 
-echo "Construyendo el servicio"
+IMAGE_NAME_2="bank-transactions"
+IMAGE_VERSION_2="latest"
+
+echo "Construyendo el servicio bank-clients"
 ./gradlew clean build -x test
 
 if [ $? -ne 0 ]; then
@@ -20,7 +23,23 @@ if [ $? -ne 0 ]; then
     exit 1
 fi
 
-echo "Levantando los servicios con Docker Compose..."
-#docker-compose up
+echo "Construyendo el servicio bank-transactions"
+cd ../bank-transactions
+./gradlew clean build -x test
+
+if [ $? -ne 0 ]; then
+    echo "Error"
+    exit 1
+fi
+
+echo "Construyendo la imagen Docker: $IMAGE_NAME_2:$IMAGE_VERSION_2"
+docker build -t $IMAGE_NAME_2:$IMAGE_VERSION_2 .
+
+if [ $? -ne 0 ]; then
+    echo "Error en la construcción de la imagen"
+    exit 1
+fi
 
 echo "Despliegue completado. La aplicación está corriendo."
+
+docker-compose up
